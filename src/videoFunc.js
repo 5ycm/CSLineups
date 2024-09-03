3c2f4888a89b36c03406dc4e44bc3b28631b8a42
5ycm <brianthe4th@gmail.com>
2024-09-03 08:25:05 -0700

    Made buttons way nicer, fixed some bugs. Still have to fix navbar buttons bug, where if you click near them, they onClick.

--- src/videoFunc.js
+++ src/videoFunc.js
@@ -7,40 +7,51 @@ function handleButtonClick(event){
 
 const buttons = document.querySelectorAll('.node');
 buttons.forEach(function(button){
-    if(button.getAttribute('data-url') != null){
-    button.onclick = handleButtonClick;
+    if (button.getAttribute('data-url') != null) {
+        button.onclick = handleButtonClick;
     }
 })
 
 function replaceSrc(url,callback,buttonClicked){
     document.querySelector('iframe').setAttribute('src',url+'?autoplay=1&mute=1');
+    document.getElementById('closerVideo').style.visibility = 'visible';
+    document.getElementById('iFrameID').style.visibility = 'visible';
+    document.getElementById('videoFrame').style.visibility = 'visible';
+
     buttons.forEach(button => {
         if (button !== buttonClicked) {
-            button.style.visibility = 'hidden';
+            button.querySelector('img').style.transform = 'scale(0)';
+            button.style.pointerEvents = 'none';     
         }
         else{
             button.querySelector('img').src = "images/reddot.png";
-            button.querySelector('img').style.transform = 'scale(1.50)';
+            button.querySelector('img').style.transform = 'scale(2.4)';
         }
     });
        
     console.log(url+' has been added');
-    callback();
-    toClose();
+    callback(toClose());
 }
 
-function setBorder(){
+function setBorder(callback){
+    document.getElementById('videoFrame').style.pointerEvents = 'auto';
     setTimeout(() => {
          document.getElementById('closerVideo').src = 'images/x-symbol.png'
-    },200);
-    setTimeout(() => {  
-        document.getElementById('videoFrame').style.border = '3px solid black';
-    },200);
+         document.getElementById('videoFrame').style.border = '3px solid white';
+    },200); 
+        document.getElementById('videoFrame').style.visibility = 'visible';
+    callback();
 }
 
 function toClose(){
 document.getElementById('closerVideo').onclick = function(){
+
+    document.getElementById('closerVideo').style.visibility = 'hidden';
+    document.getElementById('iFrameID').style.visibility = 'hidden';
+    document.getElementById('videoFrame').style.visibility = 'hidden';
+
     buttons.forEach(button=> {
+        button.style.pointerEvents = 'auto';  
         button.style.visibility = 'visible';
         button.querySelector('img').src='images/dot.png';
         button.querySelector('img').style.transform = '';
@@ -48,5 +59,6 @@ document.getElementById('closerVideo').onclick = function(){
     document.getElementById('closerVideo').src = "";
     document.getElementById('iFrameID').src = "";
     document.getElementById('videoFrame').style.border = '0px solid black';
+    document.getElementById('videoFrame').style.pointerEvents = 'none'; 
 }};
 
